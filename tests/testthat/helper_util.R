15874bfdaf10a6b5a79304985a93e414596e2490
James Melville <jlmelville@gmail.com>
2026-07-04 09:51:30 -0700

    tests for Hessian shape/symmetry/finite difference checks and problem factory helpers

--- tests/testthat/helper_util.R
+++ tests/testthat/helper_util.R
@@ -37,3 +37,107 @@ expect_gfd <- function(testfun, par, tolerance = 1e-6, tol = NULL) {
 
   expect_equal(fd, an, tolerance = tolerance)
 }
+
+problem_factory_names <- function() {
+  c(
+    "bard",
+    "beale",
+    "biggs_exp6",
+    "box_3d",
+    "brown_al",
+    "brown_bs",
+    "brown_den",
+    "broyden_band",
+    "broyden_tri",
+    "chebyquad",
+    "disc_bv",
+    "disc_ie",
+    "ex_powell",
+    "ex_rosen",
+    "freud_roth",
+    "gauss",
+    "gulf",
+    "helical",
+    "jenn_samp",
+    "kow_osb",
+    "linfun_fr",
+    "linfun_r1",
+    "linfun_r1z",
+    "meyer",
+    "osborne_1",
+    "osborne_2",
+    "penalty_1",
+    "penalty_2",
+    "powell_bs",
+    "powell_s",
+    "rosen",
+    "trigon",
+    "var_dim",
+    "watson",
+    "wood"
+  )
+}
+
+problem_factory_core_fields <- function() {
+  c("fn", "gr", "he", "fg", "x0", "fmin", "xmin")
+}
+
+get_problem_factory <- function(name) {
+  getExportedValue("funconstrain", name)
+}
+
+standard_x0 <- function(testfun) {
+  if (is.function(testfun$x0)) {
+    return(testfun$x0())
+  }
+  testfun$x0
+}
+
+hessian_fd <- function(par, gr, rel_eps = .Machine$double.eps^(1 / 4)) {
+  hessian <- matrix(0, nrow = length(par), ncol = length(par))
+
+  for (i in seq_along(par)) {
+    oldx <- par[i]
+    eps <- rel_eps
+    if (oldx != 0) {
+      eps <- abs(oldx) * rel_eps
+    }
+
+    par[i] <- oldx + eps
+    gplus <- gr(par)
+
+    par[i] <- oldx - eps
+    gminus <- gr(par)
+    par[i] <- oldx
+
+    hessian[, i] <- (gplus - gminus) / (2 * eps)
+  }
+
+  hessian
+}
+
+factory_m_metadata <- function() {
+  do.call(
+    rbind,
+    lapply(problem_factory_names(), function(name) {
+      testfun <- get_problem_factory(name)()
+      if ("m" %in% names(testfun)) {
+        return(data.frame(
+          factory = name,
+          m_present = TRUE,
+          m_type = typeof(testfun$m),
+          m_value = paste(testfun$m, collapse = ","),
+          stringsAsFactors = FALSE
+        ))
+      }
+
+      data.frame(
+        factory = name,
+        m_present = FALSE,
+        m_type = NA_character_,
+        m_value = NA_character_,
+        stringsAsFactors = FALSE
+      )
+    })
+  )
+}
---
+++ tests/testthat/test_contract.R
@@ -0,0 +1,94 @@
+test_that("factory contract list covers all exported problem factories", {
+  exported_factories <- setdiff(getNamespaceExports("funconstrain"),
+                                c("fufn", "fufnrun"))
+
+  expect_equal(sort(problem_factory_names()), sort(exported_factories))
+  expect_equal(length(problem_factory_names()), 35)
+})
+
+test_that("problem factories expose documented core fields", {
+  core_fields <- problem_factory_core_fields()
+
+  for (name in problem_factory_names()) {
+    testfun <- get_problem_factory(name)()
+
+    expect_true(all(core_fields %in% names(testfun)), info = name)
+    expect_true(is.function(testfun$fn), info = paste(name, "fn"))
+    expect_true(is.function(testfun$gr), info = paste(name, "gr"))
+    expect_true(is.function(testfun$he), info = paste(name, "he"))
+    expect_true(is.function(testfun$fg), info = paste(name, "fg"))
+    expect_true(is.numeric(testfun$fmin), info = paste(name, "fmin"))
+    expect_equal(length(testfun$fmin), 1, info = paste(name, "fmin"))
+    expect_true(length(testfun$xmin) > 0, info = paste(name, "xmin"))
+
+    x0 <- standard_x0(testfun)
+    expect_true(is.numeric(x0), info = paste(name, "x0"))
+    expect_true(length(x0) > 0, info = paste(name, "x0"))
+  }
+})
+
+test_that("fg matches fn and gr at each factory standard x0", {
+  for (name in problem_factory_names()) {
+    testfun <- get_problem_factory(name)()
+    x0 <- standard_x0(testfun)
+    fg <- testfun$fg(x0)
+
+    expect_true(all(c("fn", "gr") %in% names(fg)), info = name)
+    expect_equal(fg$fn, testfun$fn(x0), info = paste(name, "fn"))
+    expect_equal(fg$gr, testfun$gr(x0), info = paste(name, "gr"))
+  }
+})
+
+test_that("Hessians are numeric symmetric matrices at each factory standard x0", {
+  for (name in problem_factory_names()) {
+    testfun <- get_problem_factory(name)()
+    x0 <- standard_x0(testfun)
+    hessian <- testfun$he(x0)
+
+    expect_true(is.matrix(hessian), info = name)
+    expect_true(is.numeric(hessian), info = name)
+    expect_equal(dim(hessian), c(length(x0), length(x0)), info = name)
+    expect_true(isSymmetric(hessian), info = name)
+  }
+})
+
+test_that("representative Hessians match finite differences of gradients", {
+  for (name in c("bard", "beale", "rosen", "wood")) {
+    testfun <- get_problem_factory(name)()
+    x0 <- standard_x0(testfun)
+
+    expect_equal(
+      testfun$he(x0),
+      hessian_fd(x0, testfun$gr),
+      tolerance = 1e-4,
+      info = name
+    )
+  }
+})
+
+test_that("current m metadata is recorded without making it a core field", {
+  expected <- data.frame(
+    factory = problem_factory_names(),
+    m_present = c(
+      FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE,
+      FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE,
+      TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE,
+      TRUE, TRUE, FALSE, FALSE, FALSE
+    ),
+    m_type = c(
+      NA, "logical", NA, NA, NA, "logical", NA, NA, NA, "logical",
+      NA, NA, NA, NA, "logical", NA, NA, NA, "logical", NA,
+      "double", "double", "double", NA, NA, NA, NA, NA, "logical", NA,
+      "logical", "double", NA, NA, NA
+    ),
+    m_value = c(
+      NA, "NA", NA, NA, NA, "NA", NA, NA, NA, "NA",
+      NA, NA, NA, NA, "NA", NA, NA, NA, "NA", NA,
+      "100", "100", "100", NA, NA, NA, NA, NA, "NA", NA,
+      "NA", "30", NA, NA, NA
+    ),
+    stringsAsFactors = FALSE
+  )
+
+  expect_equal(factory_m_metadata(), expected)
+})
